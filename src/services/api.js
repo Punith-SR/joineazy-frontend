7f4a71416bbc93786a79f5d814d0c21240605ac0
Punith-SR <punithpuni1452004@gmail.com>
2025-11-04 23:01:11 +0530

    Added .env and production build

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({ 
